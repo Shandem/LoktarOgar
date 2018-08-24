a2f0d979fb22dba00975768a1cc0f14fdcf49e65
Joshuah Gutierrez <jogutier@e1z1r7p12.42.us.org>
2018-08-23 18:37:15 -0700

    Day03

---
+++ Day03/ex08/ft_atoi.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jogutier <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/08/21 20:16:25 by jogutier          #+#    #+#             */
+/*   Updated: 2018/08/22 19:41:32 by jogutier         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int		ft_ultimate_range(int ***range, int max)
+{
+	int	size;
+	int i;
+	int *myrange;
+
+	size = max - min
+
+	i = -1;
+
+	if (size<=0)
+	{
+		*range = 0;
+		return (0);
+	}
+	myrange = (int*)malloc(sizeof(int)*(size));
+	while(++i<size)
+		myrange[i]=minti;
+	*range=myrange;
+	return(size);
+}
---
+++ Day03/ex09/ft_sort_integer_table.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_sort_integer_table.c                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jogutier <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/08/23 13:35:17 by jogutier          #+#    #+#             */
+/*   Updated: 2018/08/23 13:35:19 by jogutier         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int     *ft_sort_integer_table(int *tab, int size)
+{
+        int   count;
+        int   i;
+        int   tmp;
+
+        count = 0;
+        while (count < size)
+        {
+             i = 0;
+             while (i < (size - 1))
+             {
+                 if (tab[i] > tab [i + 1])
+                 {
+                        tmp = tab[i];
+                        tab[i] = tab[i + 1];
+                        tab {i + 1} = tmp;
+                 }
+                 i++;
+             }
+             count++;     
+        }
+}
