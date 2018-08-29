17ffce29e827848576a6c86e5011a418b407cb86
Joshuah Gutierrez <jogutier@e1z1r4p9.42.us.org>
2018-08-29 15:41:10 -0700

    day 05

---
+++ Day05/ex05/ft_strstr.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strstr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jogutier <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/08/28 15:53:01 by jogutier          #+#    #+#             */
+/*   Updated: 2018/08/28 16:07:12 by jogutier         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+char	*ft_strstr(char *str, char *to_find)
+{
+	unsigned int pos;
+	unsigned int i;
+
+	if (!to_find)
+		return (str);
+	pos = 0;
+	while (str[pos] != '\0')
+	{
+		if (str[pos] == to_find[0])
+		{
+			i = 1;
+			while (to_find[i] != '\0' && str[pos + i] == to_find[i])
+				++i;
+			if (to_find[i] == '\0')
+				return (&str[pos]);
+		}
+		++pos;
+	}
+	return (0);
+}
