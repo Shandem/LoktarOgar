a0a4b8ee1f2a6a2d62cc3bea70a94d7317e18829
Joshuah Gutierrez <jogutier@e1z1r5p10.42.us.org>
2018-09-07 10:28:52 -0700

    day 12 00-02

---
+++ Day12/ex00/srcs/ft_display_file.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_display_file.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jogutier <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/09/06 20:05:36 by jogutier          #+#    #+#             */
+/*   Updated: 2018/09/06 20:06:28 by jogutier         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+#include <fcntl.h>
+
+void	display(char *filename)
+{
+	int		file;
+	char	character;
+
+	file = open(filename, O_RDONLY);
+	if (file < 0)
+		return ;
+	while (read(file, &character, 1))
+		write(1, &character, 1);
+	close(file);
+}
+
+int		main(int argc, char *argv[])
+{
+	if (argc == 1)
+		write(2, "File name missing.\n", 19);
+	else if (argc > 2)
+		write(2, "Too many arguments.\n", 20);
+	else
+		display(argv[1]);
+	return (0);
+}
---
+++ Day12/ex02/srcs/ft_strlen.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlen.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jogutier <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/09/06 20:12:15 by jogutier          #+#    #+#             */
+/*   Updated: 2018/09/06 20:12:42 by jogutier         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+int		ft_strlen(char *str)
+{
+	int	count;
+
+	count = 0;
+	while (str[count])
+		count++;
+	return (count);
+}
+
+void	error(char *filename)
+{
+	write(2, "tail: ", 6);
+	write(2, filename, ft_strlen(filename));
+	write(2, ": No such file or directory\n", 28);
+}
