4c1728b01c569fe5cb48f9f9c0da3ccd75fff256
Joshuah Gutierrez <gutierrezjoshuah@gmail.com>
2018-10-20 11:15:24 -0700

    better putnbr

--- Day05/ex01/ft_putnbr.c
+++ Day05/ex01/ft_putnbr.c
@@ -10,24 +10,20 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <unistd.h>
+
 void	ft_putchar(char c)
 {
-	write (1, &c, 1);
+	write(1, &c, 1);
 }
 
-void	ft_putnbr(int, nb)
+void	ft_putnbr(int n)
 {
-	unsigned int	nbr;
-
-	if(nb < 0)
+	if (n >= 10)
 	{
-		ft_putchart('-');
-		nbr = nb * -1;
+		ft_putnbr(n / 10);
+		ft_putnbr(n % 10);
 	}
 	else
-			nbr = nb;
-
-	if (nbr >=10);
-		ft_putnbr(nbr / 10);
-	ft_putchar(nbr % 10 + 48)
-
+		ft_putchar(n + '0');
+}
