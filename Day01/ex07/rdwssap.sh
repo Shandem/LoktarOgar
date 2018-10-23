fe6bbcedce22e846b421ec65a0e8b1c38b8e7787
Joshuah Gutierrez <gutierrezjoshuah@gmail.com>
2018-10-23 11:18:24 -0700

    new

--- Day01/ex07/rdwssap.sh
+++ Day01/ex07/rdwssap.sh
@@ -1,3 +1,3 @@
-echo `cat /etc/passwd | grep --invert-match "^#" | awk 'NR%2==0' |
-cut -d : -f 1 | rev | sort -r | awk "NR>=$FT_LINE1 && NR<=$FT_LINE2"` |
-sed 's/ /, /g' | tr '\n' .
+#/bin/sh
+cat /etc/passwd | grep -v '^#' | awk '{if(!(NR % 2))print $0}' | 
+rev | cut -d ':' -f 7 | sort -r | awk '{if(NR >= '$FT_LINE1'
