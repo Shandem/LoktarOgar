fb1f35d335dd3d8bc57980135debeb0c3e419ca6
Joshuah Gutierrez <joshguti@e1z1r7p33.42.us.org>
2018-11-04 12:50:37 -0800

    better makefiel

--- Day10/ex06/includes/ft_util.h
+++ Day10/ex06/includes/ft_util.h
@@ -6,21 +6,21 @@
 /*   By: jogutier <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/09/03 13:48:12 by jogutier          #+#    #+#             */
-/*   Updated: 2018/09/03 13:51:55 by jogutier         ###   ########.fr       */
+/*   Updated: 2018/11/04 11:05:38 by joshguti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef	FT_UTIL_H
+#ifndef FT_UTIL_H
 # define FT_UTIL_H
 
 void	ft_putchar(char c);
 void	ft_putnbr(int nb);
-int			calculate(int a, int b, int operation);
-int			ft_atoi(char *str);
-int			ft_add(int a, int b);
-int			ft_sub(int a, int b);
-int			ft_mul(int a, int b);
-int			ft_div(int a, int b);
-int			ft_mod(int a, int b);
+int		calculate(int a, int b, int operation);
+int		ft_atoi(char *str);
+int		ft_add(int a, int b);
+int		ft_sub(int a, int b);
+int		ft_mul(int a, int b);
+int		ft_div(int a, int b);
+int		ft_mod(int a, int b);
 
 #endif
